a750673f05873fcb562516c91b2ca59b2a33e91d
Paulina Delmore <delmore.paulina@gmail.com>
2023-02-23 20:09:38 +0000

    directors, eldest, youngest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,12 +1,26 @@
 class DirectorsController < ApplicationController
 
 def index
-  
+
 @list_of_directors = Director.all 
-
-
 
   render({ :template => "director_templates/index.html.erb" })
 end
+
+
+def eldest
+@oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc}).at(0)
+
+
+  render({ :template => "director_templates/eldest.html.erb" })
+end
+
+
+def youngest
+  @youngest = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
+
+  render({ :template => "director_templates/youngest.html.erb" })
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "application", :action => "homepage" })
 
-get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
 
 end
